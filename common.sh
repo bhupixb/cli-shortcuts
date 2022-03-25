2be536c6387eba4b92107948c31235bbf6cccf08
Bhupendra Yadav <30554307+bhupixb@users.noreply.github.com>
2022-03-25 23:24:50 +0530

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,7 +1,13 @@
-
+# Gives a quick summary on how to do something with a cli tool
 how() {
     # set -x
     local args=$@
     local cheat_sh_format="$(echo $args | sed "s/ /\//g")"
     curl "cheat.sh/${cheat_sh_format}"
+}
+
+# extract json object with matchings keys only
+json() {
+    #gron | grep -Ew 'spec\.containers\[.\]\.(resources|name|env)' | gron --ungron
+    gron | grep $@ | grep -v 'kubectl.kubernetes.io/last-applied-configuration' | gron --ungron
 }
